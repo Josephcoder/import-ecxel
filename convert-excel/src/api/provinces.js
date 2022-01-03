bb8bffbc49edde4845a46bc77488046f8f82b83e
gabin100 <ij.gabin123@gmail.com>
2022-01-03 14:13:18 +0200

    document

--- convert-excel/src/api/provinces.js
+++ convert-excel/src/api/provinces.js
@@ -3,32 +3,54 @@ const Provinces = require('../models/provinces');
 
 const router = express.Router();
 
+const provinceObject = [
+  {
+    name: 'Kigali',
+    full_name: 'Umujyi wa Kigali',
+    code: 1,
+  },
+  {
+    name: 'South',
+    full_name: 'Amajyepfo',
+    code: 2,
+  },
+  {
+    name: 'West',
+    full_name: 'Iburengerazuba',
+    code: 3,
+  },
+  {
+    name: 'North',
+    full_name: 'Amajyaruguru',
+    code: 4,
+  },
+  {
+    name: 'East',
+    full_name: 'Iburasirazuba',
+    code: 5,
+  },
+];
+
 router.post('/', async (req, res) => {
   try {
-    const { name, fullname, code } = req.body;
-
-    const elementExist = await Provinces.findOne({
-      province_code: code,
-    }).exec();
-
-    if (!elementExist) {
-      const province = Provinces({
-        province_name: name,
-        full_name: fullname,
-        province_code: code,
-      });
-
-      await province.save();
-
-      res.json({
-        message: 'Success',
-        data: { province },
-      });
-    } else {
-      res.status(400).json({
-        message: 'Failed',
-      });
+    for (let element of provinceObject) {
+      const elementExist = await Provinces.findOne({
+        province_code: element.code,
+      }).exec();
+      if (!elementExist) {
+        const province = Provinces({
+          province_name: element.name,
+          full_name: element.full_name,
+          province_code: element.code,
+        });
+        await province.save();
+      }
     }
+
+    res.json({
+      message: 'Success',
+      data: provinceObject,
+    });
   } catch (error) {
     console.log(error);
   }
